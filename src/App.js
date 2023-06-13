6721f17129b4ee6b254fb87f00cebd2c31e29a8d
ANAGIT22 <103462670+ANAGIT22@users.noreply.github.com>
2023-06-13 13:19:22 -0300

    renderizado de Elementos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Componente from './components/Componente';
 import Propiedades from './components/Propiedades';
 import Estado from './components/Estado';
 import RenderizadoCondicional from './components/RenderizadoCondicional';
+import RenderizadoElementos from './components/RenderizadoElementos';
+
+
 function App() {
   return (
     <div className="App">
@@ -42,6 +45,8 @@ function App() {
        <Estado/>
        <hr/>
        <RenderizadoCondicional/>
+       <hr/>
+       <RenderizadoElementos/>
 
       </section>
       </header>
